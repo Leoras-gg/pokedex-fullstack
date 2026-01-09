0c172b9156ec02d0b22c2387ffcd344fd9ce4735
Leandro Horas Pereira <leandro.cc.horas@gmail.com>
2026-01-09 05:38:05 -0300

    feat(backend): connect application to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import app from "./src/app.js";
+import { connectDatabase } from "./src/config/database.js";
 
 const PORT = process.env.PORT || 3001;
 
+connectDatabase();
+
 app.listen(PORT, () => {
-    console.log(`Server na porta ${PORT}`);
+  console.log(`Server na porta ${PORT}`);
 });

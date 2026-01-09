bc48906a91261ccf3fce5c40d6cabf535e9d4f8d
Leandro Horas Pereira <leandro.cc.horas@gmail.com>
2026-01-09 07:07:04 -0300

    feat(backend): add user model with password hashing

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    favorites: {
+      type: [String],
+      default: []
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("User", UserSchema);

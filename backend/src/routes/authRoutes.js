42ff8530875ab970705cf3111bcf58cfd2d350cd
Leandro Horas Pereira <leandro.cc.horas@gmail.com>
2026-01-15 05:45:16 -0300

    refactor(auth) include comment on code

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController.js";
 
+/**
+ * Rotas de autenticação
+ * - Apenas gestão de usuários
+ */
 const router = Router();
 
 router.post("/register", register);

6ca3efdf3d14c3dab78d763f411f33131c691c36
Leandro Horas Pereira <leandro.cc.horas@gmail.com>
2026-01-15 23:08:05 -0300

    feat(pokemon-card): enhance card layout and add type icons for better visual representation

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,28 +1,35 @@
 import "../styles/pokemon-card.css";
+import typeIcons from "../utils/typeIcons";
 import "../styles/types.css";
 
-export default function PokemonCard({ pokemon, isFavorite, onToggleFavorite }) {
-  const mainType = pokemon.types?.[0] || "normal";
 
+export default function PokemonCard({ pokemon, isFavorite, onToggleFavorite }) {
   return (
-    <div className={`pokemon-card type-${mainType}`}>
+    <div className={`pokemon-card`}>
+      {/* Número do Pokémon */}
       <span className="pokemon-id">#{pokemon.id}</span>
 
+      {/* Tipos no canto superior direito */}
+      <div className="pokemon-types">
+        {pokemon.types.map(type => (
+          <img
+            key={type}
+            src={typeIcons[type]}
+            alt={type}
+            title={type.charAt(0).toUpperCase() + type.slice(1)}
+            className={`type-icon ${type}`}
+          />
+        ))}
+      </div>
+
+      {/* Sprite do Pokémon */}
       <div className="pokemon-image">
         <img src={pokemon.sprite} alt={pokemon.name} />
       </div>
 
-      <div className="pokemon-info">
-        <h3>{pokemon.name}</h3>
-
-        <div className="pokemon-types">
-          {pokemon.types.map(type => (
-            <span key={type} className={`type-badge ${type}`}>
-              {type}
-            </span>
-          ))}
-        </div>
-
+      {/* Rodapé: Nome + estrela de favorito */}
+      <div className="pokemon-footer">
+        <h3 className="pokemon-name">{pokemon.name}</h3>
         <button
           className={`favorite-btn ${isFavorite ? "active" : ""}`}
           onClick={() => onToggleFavorite(pokemon.id)}
---
+++ frontend/src/utils/typeIcons.js
@@ -0,0 +1,41 @@
+import normal from "../assets/type-icons/normal.svg";
+import grass from "../assets/type-icons/grass.svg";
+import fire from "../assets/type-icons/fire.svg";
+import water from "../assets/type-icons/water.svg";
+import electric from "../assets/type-icons/electric.svg";
+import ice from "../assets/type-icons/ice.svg";
+import fighting from "../assets/type-icons/fighting.svg";
+import poison from "../assets/type-icons/poison.svg";
+import ground from "../assets/type-icons/ground.svg";
+import flying from "../assets/type-icons/flying.svg";
+import psychic from "../assets/type-icons/psychic.svg";
+import bug from "../assets/type-icons/bug.svg";
+import rock from "../assets/type-icons/rock.svg";
+import ghost from "../assets/type-icons/ghost.svg";
+import dragon from "../assets/type-icons/dragon.svg";
+import dark from "../assets/type-icons/dark.svg";
+import steel from "../assets/type-icons/steel.svg";
+import fairy from "../assets/type-icons/fairy.svg";
+
+const typeIcons = {
+  normal,
+  grass,
+  fire,
+  water,
+  electric,
+  ice,
+  fighting,
+  poison,
+  ground,
+  flying,
+  psychic,
+  bug,
+  rock,
+  ghost,
+  dragon,
+  dark,
+  steel,
+  fairy
+};
+
+export default typeIcons;
